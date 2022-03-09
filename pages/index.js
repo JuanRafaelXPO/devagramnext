74ca2d7c3030899b03cf8f5499d71f408ceceb89
Juan Rafael <juanrafaelxpo@outlook.com>
2022-03-09 16:50:15 -0300

    Componentes de botao, avatar, uploadDeImagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import { useState, useRef } from "react";
+import Avatar from "../components/avatar";
+import Botao from "../components/botao";
+import UploadImagem from "../components/uploadImagem";
+
 export default function Home() {
+  const [imagem, setImagem] = useState(null)
+  const referenciaInput = useRef(null);
+  
+  console.log(imagem)
+  
   return (
-    <h1>hello</h1>
+    <>
+      <h1>hello</h1>
+      <button onClick={() => referenciaInput?.current?.click()}>abir seletor de arquivos</button>
+      <UploadImagem 
+        setImagem={setImagem} 
+        imagemPreview={imagem?.preview} 
+        aoSetarAReferencia={(ref) => referenciaInput.current = ref}
+      />
+      <Avatar />
+      <Botao texto={'login'} cor='primaria' manipularClique={() => console.log('botao clicado')}/>
+    </>
   )
 }
